2a01454acca417c17ff9795bbcae6045323841fb
Max Frank <max.frank@czbiohub.org>
2025-08-25 17:27:45 -0700

    add spectronaut reader

--- protdata/io/__init__.py
+++ protdata/io/__init__.py
@@ -1,6 +1,13 @@
-from .maxquant_loader import read_maxquant
 from .diann_loader import read_diann
 from .fragpipe_loader import read_fragpipe
+from .maxquant_loader import read_maxquant
 from .mztab_loader import read_mztab
+from .spectronaut_loader import read_spectronaut
 
-__all__ = ["read_maxquant", "read_diann", "read_fragpipe", "read_mztab"]
+__all__ = [
+    "read_maxquant",
+    "read_diann",
+    "read_fragpipe",
+    "read_mztab",
+    "read_spectronaut",
+]
